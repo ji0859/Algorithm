e3240a56ab8bcac6dae6dd92864e17ad62788dc6
Eunji Lee <ji0859@kookmin.ac.kr>
2020-12-29 02:58:07 +0900

    K번째수

---
+++ programmers/level1/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        ans1 = array[commands[i][0]-1:commands[i][1]]
+        ans1.sort()
+        answer.append(ans1[commands[i][2]-1])
+    return answer
