acd3ab6505430249a8bb8dc6f8344aad8a29b715
Eunji Lee <ji0859@kookmin.ac.kr>
2021-01-01 19:50:41 +0900

    같은 숫자는 싫어

---
+++ programmers/level1/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    answer.append(arr[0])
+    for i in range(len(arr)-1):
+        if (arr[i] != arr[i+1]):
+            answer.append(arr[i+1])
+    print('Hello Python')
+    return answer
